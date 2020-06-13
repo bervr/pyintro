1baa81835c9bc1e1ea041dc2d3a898192411bf88
bervr <bervrr@gamail.com>
2020-06-13 22:05:09 +0400

    pep8

--- 2_exchange.py
+++ 2_exchange.py
@@ -2,12 +2,12 @@
 # Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
 # При нечетном количестве элементов последний сохранить на своем месте.
 # Для заполнения списка элементов необходимо использовать функцию input()
-#second_list = input('введите список')
+# second_list = input('введите список')
 second_list = [11, 12.74, False, '', "Non", -99, 0, 'Hi', 12]
 result_list = []
-for el in range(len(second_list)+1):
-    if not el%2:
-        temp_list = second_list[el:el+2]
+for el in range(len(second_list) + 1):
+    if not el % 2:
+        temp_list = second_list[el:el + 2]
         temp_list.reverse()
         result_list.extend(temp_list)
 print(result_list)
