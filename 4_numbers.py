cc8986c5dc77fd04133c999ac69644c2565d3919
philippov <bervr@mail.ru>
2020-06-23 17:08:54 +0400

    done with strange

--- 4_numbers.py
+++ 4_numbers.py
@@ -6,11 +6,12 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
-
+my_text_block = ''
 rus = ['один', 'два', 'три', 'четыре']
 with open("text_4.txt", "r", encoding='utf-8') as my_file:
     for string in my_file:
         number = string.split()
-        with open("new_4.txt", "a", encoding='utf-8') as new_file:
-            print(f'{rus[int(number[2]) - 1].capitalize()} - {number[2]}',file=new_file)
+        my_text_block += "".join([f'{rus[int(number[2]) - 1].capitalize()} - {number[2]}\n'])
+with open("new_4.txt", "a", encoding='utf-8') as new_file:
+    new_file.write(my_text_block)
 
