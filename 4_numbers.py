84ec138e3cbbe9cd15241e2184f83fc055738e7e
bervr <bervrr@gamail.com>
2020-06-24 11:31:35 +0400

    change list to googletrans

--- 4_numbers.py
+++ 4_numbers.py
@@ -12,8 +12,8 @@ translator = googletrans.Translator()
 with open("text_4.txt", "r", encoding='utf-8') as my_file:
     for string in my_file:
         number = string.split()
-        my_text_block += "".join([f'{(translator.translate(number[0],dest="ru").text).capitalize()} - {number[2]}\n'])
+        my_text_block += "".join([f'{translator.translate(number[0],dest="ru").text.capitalize()} - {number[2]}\n'])
 with open("new_4.txt", "a", encoding='utf-8') as new_file:
     new_file.write(my_text_block)
-#pip install googletrans
+# pip install googletrans
 
