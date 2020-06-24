5cdaea71e40d55edbc0621c19151c8fa3ca56027
bervr <bervrr@gamail.com>
2020-06-24 11:29:57 +0400

    change list to googletrans

--- 4_numbers.py
+++ 4_numbers.py
@@ -7,11 +7,13 @@
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
 my_text_block = ''
-rus = ['один', 'два', 'три', 'четыре']
+import googletrans
+translator = googletrans.Translator()
 with open("text_4.txt", "r", encoding='utf-8') as my_file:
     for string in my_file:
         number = string.split()
-        my_text_block += "".join([f'{rus[int(number[2]) - 1].capitalize()} - {number[2]}\n'])
+        my_text_block += "".join([f'{(translator.translate(number[0],dest="ru").text).capitalize()} - {number[2]}\n'])
 with open("new_4.txt", "a", encoding='utf-8') as new_file:
     new_file.write(my_text_block)
+#pip install googletrans
 
