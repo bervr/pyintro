1b46b3184e50b302555f03018bc00dcd74b0311d
philippov <bervr@mail.ru>
2020-06-25 12:24:06 +0400

    PEP8

--- 4_numbers.py
+++ 4_numbers.py
@@ -6,14 +6,13 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
-my_text_block = ''
 import googletrans
+my_text_block = ''
 translator = googletrans.Translator()
 with open("text_4.txt", "r", encoding='utf-8') as my_file:
     for string in my_file:
         number = string.split()
-        my_text_block += "".join([f'{translator.translate(number[0],dest="ru").text.capitalize()} - {number[2]}\n'])
+        my_text_block += "".join([f'{translator.translate(number[0], dest="ru").text.capitalize()} - {number[2]}\n'])
 with open("new_4.txt", "a", encoding='utf-8') as new_file:
     new_file.write(my_text_block)
 # pip install googletrans
-
--- 5_random.py
+++ 5_random.py
@@ -2,9 +2,10 @@
 # # Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 
 import random
+
 my_text_block = ' '.join(map(str, [random.randint(-1000, 1000) for el in range(15)])) + ' '
 with open("new_5.txt", "a+", encoding='utf-8') as new_file:
     new_file.writelines(my_text_block)
     new_file.seek(0)
     full_numbers = new_file.read().split()
-print(f'сумма чисел в файле {sum(map(int,full_numbers))}')
+print(f'сумма чисел в файле {sum(map(int, full_numbers))}')
